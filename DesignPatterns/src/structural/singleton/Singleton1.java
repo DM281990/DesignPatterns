91ed55fd0ff1d23fe9611f1f5683c472e011344c
DIWAKAR_MISHRA_YODLEE_PC <diwakarmishra1990@gmail.com>
2024-12-20 14:15:37 +0530

    Singleton Class

---
+++ DesignPatterns/src/structural/singleton/Singleton1.java
@@ -0,0 +1,15 @@
+package structural.singleton;
+
+/**
+ * Checking null
+ */
+public class Singleton1 {
+    private static  Singleton1 singleton1;
+    private Singleton1(){};
+    private static Singleton1 getInstance(){
+        if(singleton1==null){
+            singleton1 = new Singleton1();
+        }
+        return singleton1;
+    }
+}
---
+++ DesignPatterns/src/structural/singleton/Singleton4.java
@@ -0,0 +1,19 @@
+package structural.singleton;
+
+/**
+ * Double Check lock
+ */
+public class Singleton4 {
+    private static Singleton4 singleton4;
+    private Singleton4(){};
+    private  static Singleton4 getInstance(){
+        if(singleton4==null){
+            synchronized (Singleton4.class){
+                if(singleton4==null){
+                    singleton4=new Singleton4();
+                }
+            }
+        }
+        return singleton4;
+    }
+}
