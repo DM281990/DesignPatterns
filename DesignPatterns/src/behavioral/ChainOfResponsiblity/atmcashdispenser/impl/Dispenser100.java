a0fca38afc0688b958f484b889b7feb2caa0cd9a
DIWAKAR_MISHRA_YODLEE_PC <diwakarmishra1990@gmail.com>
2024-12-21 23:46:43 +0530

    Chain Of Responsibility

--- DesignPatterns/src/behavioral/ChainOfResponsiblity/atmcashdispenser/impl/Dispenser100.java
+++ DesignPatterns/src/behavioral/ChainOfResponsiblity/atmcashdispenser/impl/Dispenser100.java
@@ -5,10 +5,9 @@ import behavioral.ChainOfResponsiblity.atmcashdispenser.CashDispenser;
 public class Dispenser100 implements CashDispenser {
     @Override
     public void dispenseCash(int amount) {
-        public void dispenseCash(int amount) {
             int noOfNotes =0;
             noOfNotes = amount/100;
             System.out.println("DISPENSING" + noOfNotes+" 100 NOTES for");
-        }
+        
     }
 }
